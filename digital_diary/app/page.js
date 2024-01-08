d2d8a43dcd71ef09b27861109c7ba13b0b9ffa3d
nidackmk <nidackmk8@gmail.com>
2024-01-08 21:16:31 +0300

    Redesigned

--- digital_diary/app/page.js
+++ digital_diary/app/page.js
@@ -14,7 +14,7 @@ import { db } from './firebase';
 
 export default function Home() {
   const [items, setItems] = useState([]);
-  const [newItem, setNewItem] = useState({ metin: ''});
+  const [newItem, setNewItem] = useState({ metin: '' });
 
 
   // Add item to database
@@ -25,28 +25,28 @@ export default function Home() {
         metin: newItem.metin.trim(),
         createdAt: serverTimestamp(), // Tarih bilgisini ekledik
       });
-      setNewItem({ metin: ''});
+      setNewItem({ metin: '' });
     }
   };
 
 
- // Read items from database
-useEffect(() => {
-  const q = query(collection(db, 'items'));
-  const unsubscribe = onSnapshot(q, (querySnapshot) => {
-    let itemsArr = [];
+  // Read items from database
+  useEffect(() => {
+    const q = query(collection(db, 'items'));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let itemsArr = [];
 
-    querySnapshot.forEach((doc) => {
-      itemsArr.push({ ...doc.data(), id: doc.id });
+      querySnapshot.forEach((doc) => {
+        itemsArr.push({ ...doc.data(), id: doc.id });
+      });
+
+      // Sıralama işlemini burada gerçekleştiriyoruz
+      itemsArr.sort((a, b) => b.createdAt - a.createdAt);
+
+      setItems(itemsArr);
+      return () => unsubscribe();
     });
-
-    // Sıralama işlemini burada gerçekleştiriyoruz
-    itemsArr.sort((a, b) => b.createdAt - a.createdAt);
-
-    setItems(itemsArr);
-    return () => unsubscribe();
-  });
-}, []);
+  }, []);
 
 
   // Delete items from database
@@ -55,55 +55,61 @@ useEffect(() => {
   };
 
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between sm:p-24 p-4'>
-      <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm '>
-        <h1 className='text-4xl p-4 text-center'>Digital Diary</h1>
-        <div className='bg-slate-800 p-4 rounded-lg'>
-          <form className='grid grid-cols-6 items-center text-black'>
-            <textarea 
-              value={newItem.metin}
-              onChange={(e) => setNewItem({ ...newItem, metin: e.target.value })}
-              className='textarea textarea-success col-span-3 p-3 border mb-3'
-              type='text'
-              placeholder='Dear Diary, '
-            />
-          </form> 
-          <button
-              onClick={addItem}
-              className='btn btn-outline btn-success'
-              type='submit'
+    <main className='flex min-h-screen w-full flex-col items-center justify-between '>
+      <div className='w-full px-0 items-center justify-between font-mono text-sm h-screen h-1/2 mb-10' style={{ backgroundImage: "url('/app/appbar.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
+        <div className="bg-success w-full flex justify-center items-end px-0 h-full">
+          <h1 className='text-2xl text-white'>Dear Diary</h1>
+        </div>
+      </div>
+
+      <div className=' p-10 rounded-lg w-full'>
+        <form className='grid grid-cols-6 items-center text-black'>
+          <textarea
+            value={newItem.metin}
+            onChange={(e) => setNewItem({ ...newItem, metin: e.target.value })}
+            className='textarea textarea-success col-span-6 p-3 border mb-3 h-40'
+            type='text'
+            placeholder='Dear Diary, '
+          />
+        </form>
+        <button
+          onClick={addItem}
+          className='btn btn-outline btn-success'
+          type='submit'
+        >
+          Kaydet
+        </button>
+        <ul className="flex flex-wrap -mx-2">
+          {items.map((item, id) => (
+            <li
+              key={id}
+              className='my-4 w-full sm:w-1/2 lg:w-1/3 px-2'
             >
-              Kaydet
-            </button>
-          <ul>
-            {items.map((item, id) => (
-              <li
-                key={id}
-                className='my-4 w-full flex justify-between bg-slate-950'
-              >
-                <div className='p-4 w-full flex justify-between'>
-                  <span className='capitalize'>{item.metin}</span>
-                  <span className='text-sm'>{item.createdAt && item.createdAt.toDate().toLocaleString()}</span> {/* Ekledik */}
+              <div className='p-4 w-full flex flex-col justify-between bg-slate-950 h-full'>
+                <span className='text-sm'>{item.createdAt && item.createdAt.toDate().toLocaleString()}</span>
+                <div className="flex justify-between items-center">
+                  <span className='capitalize mb-2'>{item.metin}</span>
+                  <button
+                    onClick={() => deleteItem(item.id)}
+                    className='p-2 border-l-2 border-slate-900 hover:bg-slate-900'
+                  >
+                    X
+                  </button>
                 </div>
-                <button
-                  onClick={() => deleteItem(item.id)}
-                  className='ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16'
-                >
-                  X
-                </button>
-              </li>
-            ))}
-          </ul>
-          {items.length < 1 ? (
-            ''
-          ) : 
+              </div>
+            </li>
+          ))}
+        </ul>
+
+        {items.length < 1 ? (
+          ''
+        ) :
           (
             <div className='flex justify-between p-3'>
-              
+
             </div>
           )
-          }
-        </div>
+        }
       </div>
     </main>
   );
