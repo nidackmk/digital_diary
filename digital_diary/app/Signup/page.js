1f70013aae8a91c5be26d557553a25ee047de610
nidackmk <nidackmk8@gmail.com>
2024-01-13 01:29:52 +0300

    Completed authorization

--- digital_diary/app/Signup/page.js
+++ digital_diary/app/Signup/page.js
@@ -1,24 +1,22 @@
 'use client'
 import { useState } from 'react';
-import {useSignInWithEmailAndPassword} from 'react-firebase-hooks/auth'
+import {useCreateUserWithEmailAndPassword} from 'react-firebase-hooks/auth'
 import {auth} from '@/app/firebase'
-import { useRouter } from 'next/navigation';
 
-const SignIn = () => {
+const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
-  const router = useRouter()
+  const [createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth);
 
-  const handleSignIn = async () => {
+  const handleSignUp = async () => {
     try {
-        const res = await signInWithEmailAndPassword(email, password);
-        console.log({res});
+        const res = await createUserWithEmailAndPassword(email, password)
+        console.log({res})
         sessionStorage.setItem('user', true)
         setEmail('');
-        setPassword('');
-        router.push('/')
-    }catch(e){
+        setPassword('')
+
+    } catch(e){
         console.error(e)
     }
   };
@@ -26,7 +24,7 @@ const SignIn = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900">
       <div className="bg-gray-800 p-10 rounded-lg shadow-xl w-96">
-        <h1 className="text-white text-2xl mb-5">Sign In</h1>
+        <h1 className="text-white text-2xl mb-5">Sign Up</h1>
         <input 
           type="email" 
           placeholder="Email" 
@@ -42,14 +40,14 @@ const SignIn = () => {
           className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
         />
         <button 
-          onClick={handleSignIn}
+          onClick={handleSignUp}
           className="w-full p-3 bg-indigo-600 rounded text-white hover:bg-indigo-500"
         >
-          Sign In
+          Sign Up
         </button>
       </div>
     </div>
   );
 };
 
-export default SignIn;
+export default SignUp;
--- digital_diary/app/firebase.js
+++ digital_diary/app/firebase.js
@@ -4,6 +4,8 @@ import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore } from "firebase/firestore";
+import {getAuth} from 'firebase/auth'
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -20,4 +22,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export const db= getFirestore(app);
+const auth = getAuth(app)
+const db= getFirestore(app);
+
+export {db, auth, app};
